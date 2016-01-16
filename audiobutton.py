f6f2fcf0207414afbcf0e58d1f02ff432d44a3ea
Steve M <steve@warning.ca>
2016-01-16 07:57:33 +0000

    Cleanup + easter egg!

--- audiobutton.py
+++ audiobutton.py
@@ -4,19 +4,21 @@ from signal import pause
 from time import sleep
 import os
 
+# A hack to get around a known bug
+# https://github.com/RPi-Distro/python-gpiozero/issues/50
 button1 = None
 while not button1:
     try:
-        button1 = Button(16)
+        button1 = Button(16, bounce_time=0.2)
     except RuntimeError:
         pass
 
-num = -1
-prnt = 0
+num = 0
+playloop = 0
 last = False
 
 def audioplay():
-    print("playing sequence %s" % num)
+    print("playing song %s" % num)
     os.system("pkill play")
     sleep(.2)
     os.system("/home/pi/tones.sh %s" % num)
@@ -26,28 +28,39 @@ while True:
 
     input_value = button1.is_pressed
 
-    if (input_value == True) and (last == False):
-        last = True
-        prnt = 1
-        num += 1
-        sleep(.01)
-        continue
+    if   (input_value == True) and (last == False):
+         last = True
+         playloop = 1
+         num += 1
+         sleep(.01)
+    elif (input_value == False) and (last == True):
+         last = False
+         sleep(.01)
+    elif (input_value == True) and (last == True):
+          if (playloop == 1):
+            if (num == 1):
+               button_press_timer = 0
+               while True:
+                   if (button1.is_pressed == True) : # while button is still pressed down
+                      button_press_timer += 1 # keep counting until button is released
+                      print("button hold count %s" % button_press_timer)
+                      sleep(1)
+                   else: # button is released, figure out for how long
+                       if (button_press_timer > 4) : # pressed for > 4 seconds
+                           num = 99
+                           audioplay()
+                           num = 0
+                       elif (button_press_timer < 4) : # pressed for < 4 seconds
+                           audioplay()
+                       break
+                       button_press_timer = 0
+            elif (num >= 2) and (num <= 10):
+                 audioplay()
+            elif (num == 11):
+                 print("Stopping on count %s" % num)
+                 os.system("pkill play")
+                 print("Press button again to start over")
+                 num = 0
 
-    if (input_value == False) and (last == True):
-        last = False
-        sleep(.01)
-        continue
-
-    if (input_value == True) and (last == True):
-        if (prnt == 1):
-            if (num >= 11):
-                num = 0
-            if (num <= 9):
-                audioplay()
-            if (num >= 10):
-                os.system("pkill play")
-            prnt = 0
-            sleep(.01)
-        continue
-
+            playloop = 0
 pause()
--- tones.sh
+++ tones.sh
@@ -1,37 +1,40 @@
 #!/bin/bash
 
-PLAY="/usr/bin/play -q -n"
+PLAY="/usr/bin/play -q"
 
 case "$1" in
-	0)
-	$PLAY synth pl E2 pl A2 pl D3 pl G3 pl B3 pl E4 delay 0 .05 .1 .15 .2 .25 \
+	1)
+	$PLAY -n synth pl E2 pl A2 pl D3 pl G3 pl B3 pl E4 delay 0 .05 .1 .15 .2 .25 \
 	remix - fade 0 4 .1 norm -1 gain 3 repeat 99 &
 	;;
-	1)
-	$PLAY -c 2 synth 8 sine 30-120 fade 0 0 0.1 repeat 99 &
-	;;
 	2)
-	$PLAY -c 2 synth 8 sine 100-2000 repeat 99 &
+	$PLAY -n -c 2 synth 8 sine 30-120 fade 0 0 0.1 repeat 99 &
 	;;
 	3)
-	$PLAY -c 2 synth 8 sine 30-20000 repeat 99 &
+	$PLAY -n -c 2 synth 8 sine 100-2000 repeat 99 &
 	;;
 	4)
-	$PLAY -c 2 synth sine 60 &
+	$PLAY -n -c 2 synth 8 sine 30-20000 repeat 99 &
 	;;
 	5)
-	$PLAY -c 2 synth sine 100 &
+	$PLAY -n -c 2 synth sine 60 &
 	;;
 	6)
-	$PLAY -c 2 synth sine 500 &
+	$PLAY -n -c 2 synth sine 100 &
 	;;
 	7)
-	$PLAY -c 2 synth sine 1000 &
+	$PLAY -n -c 2 synth sine 500 &
 	;;
 	8)
-	$PLAY -c 2 synth whitenoise &
+	$PLAY -n -c 2 synth sine 1000 &
 	;;
 	9)
-	$PLAY -c 2 synth pinknoise &
+	$PLAY -n -c 2 synth whitenoise &
+	;;
+	10)
+	$PLAY -n -c 2 synth pinknoise &
+	;;
+	99)
+	$PLAY $HOME/EasterEgg.mp3 &
 	;;
 esac
